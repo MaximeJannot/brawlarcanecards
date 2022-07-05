96969785baf9a43a2c9ec67e47186b986d771042
Maxime Jannot <max.jannot@gmail.com>
2022-07-05 17:49:42 +0200

    0.2.0 - update script for count of life points

--- assets/script.js
+++ assets/script.js
@@ -1,10 +1,11 @@
+var mage = ["artful", "astral", "blood", "draconic", "flesh", "green", "luminous", "metallurgist", "mermancer", "necromancer", "noble"];
+var minion = ["phantasm", "void", "bloodimp", "drake", "mutant", "forest", "brightling", "automaton", "denizen", "undead", "living"];
 
 function managementHealth(health, character) {
 	var maxHealth = health;
 	var looseHealth = document.getElementById('loose_health_'+character);
 	var gainHealth = document.getElementById('gain_health_'+character);
 	var totalHealth = document.getElementById('total_health_'+character);
-
 	looseHealth.onclick = function() {
 		if (maxHealth > 0) {
 			--maxHealth;
@@ -19,4 +20,20 @@ function managementHealth(health, character) {
 	}
 }
 
-managementHealth(15, 'mage')
+for (i = 0; i < mage.length; i++) {
+	var idmage = 'mage-'+mage[i];
+	managementHealth(15, idmage)
+}
+for (i = 0; i < minion.length; i++) {
+	var idminion = 'minion-'+minion[i]+'-1';
+	console.log(idminion);
+	managementHealth(4, idminion)
+}
+for (i = 0; i < minion.length; i++) {
+	var idminion = 'minion-'+minion[i]+'-2';
+	managementHealth(4, idminion)
+}
+for (i = 0; i < minion.length; i++) {
+	var idminion = 'minion-'+minion[i]+'-3';
+	managementHealth(4, idminion)
+}
